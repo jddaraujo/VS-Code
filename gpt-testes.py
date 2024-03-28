6812a99a4600e92b3375dc4538e2652ebb8323c9
jddaraujo <jddaraujo10@gmail.com>
2024-03-28 00:59:12 +0000

    teste

---
+++ gpt-testes.py
@@ -0,0 +1,8 @@
+# Solicita ao usuário que insira seu nome
+nome = input("Por favor, insira seu nome: ")
+
+# Mensagem de saudação personalizada
+mensagem = f"Olá, {nome}! Seja bem-vindo ao seu programa teste."
+
+# Imprime a mensagem de saudação
+print(mensagem)
